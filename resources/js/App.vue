03e2753faf52b5b615946f9f77b1cea3429cad4e
allandereal <ahullan@gmail.com>
2023-01-21 11:26:13 +0300

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="min-h-screen bg-gray-100 py-4 sm:pt-0">
+        <div class="max-w-6xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div class="flex flex-col items-start gap-4">
+                <h3 class="text-gray-700 font-semibold text-xl">My Forms</h3>
+                <button class="flex items-center justify-between gap-2 text-white rounded-sm py-1 px-2 bg-gray-900">
+                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    <span class="font-sm">Create form</span>
+                </button>
+                <hr class="border-b border-slate-200 w-full"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
